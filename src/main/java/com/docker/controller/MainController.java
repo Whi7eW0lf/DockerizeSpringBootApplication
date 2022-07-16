27b0744b7b5974b0ba724a53d1efebc62685af40
whi7ew0lf <salihmusov1996@gmail.com>
2022-07-16 23:54:38 +0300

    Added maven config

---
+++ src/main/java/com/docker/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.docker.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> helloDocker() {
+        return ResponseEntity.ok("Hello Docker!");
+    }
+
+}
